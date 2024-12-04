7749fa9ad99c462bb2849133e7f07243b6f54ca9
Timothy Li <timothy93li@gmail.com>
2024-12-04 14:34:12 +0000

    finished ticket 3

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -1,28 +1,15 @@
 function countNumberOfNinesInNumber(number) {
   let count = 0;
+  // assigning new variable to convert number to a string
+  let numberAsString = number.toString();
 
-  for (let i = 0; i < number.length; i++) {
-    const digit = number[i];
+
+  for (let i = 0; i < numberAsString.length; i++) {
+    const digit = numberAsString[i];
     if ("9" === digit) {
       count++;
     }
   }
 
-//  return count;
-}
-
-// declaring function called countNumberOfNinesInNumber, which takes in a parameter called number
-// define a variable called count to track the number of 9s in number, starting at 0
-
-// for loop, iterates through number
-// declaring constant variable called digit
-// 
-
-console.log(countNumberOfNinesInNumber(99.999));
-
-
-let word = [1, 2, 3];
-
-for (let i = 0; i < word.length; i++) {
-  console.log(i);
+  return count;
 }
