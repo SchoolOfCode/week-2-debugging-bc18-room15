2f5b6cbdc8bab55cc045307339b9f1c795f3b1bb
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2024-12-04 15:40:38 +0000

    attempted ticket 5, not complete

--- ticket-5/index.js
+++ ticket-5/index.js
@@ -1,12 +1,11 @@
-const shoppingCart = [
+const shoppingCart = [    // a constant variable called shoppingCart
   {
-    product: "Bananas",
+    product: "Bananas",  // three objects with product, price and quantity
     price: 0.5,
     quantity: 6,
   },
   {
     product: "Apples",
-
     price: 0.7,
     quantity: 8,
   },
@@ -17,11 +16,11 @@ const shoppingCart = [
   },
 ];
 
-let totalCost = 0;
+let totalCost = 0;              // variable let called totalCost which has a value of O
 
-for (let i = 0; i <= shoppingCart.length; i++) {
-  const item = shoppingCart[i];
-  const costForItem = item.price * item.quantity;
+for (let i = 0; i <= shoppingCart.length; i++) {     // for loop with iteration 
+  let item = shoppingCart[i];                      // constant variable is declared called item, which has the value of hoppingCart[i]
+  const costForItem = item.price * item.quantity;  // 
   totalCost += costForItem;
 }
 
@@ -31,3 +30,10 @@ const formatter = new Intl.NumberFormat("en-GB", {
 });
 
 console.log(`The total cost for this cart is ${formatter.format(totalCost)}`);
+
+//  The total cost for this cart is £23.60
+// One of JavaScript's flexible behaviours is that accessing a non-existent property on an object simply evaluates to undefined. 
+//However, this lenient behaviour does not apply when trying to access a property on an undefined or null value (instead JavaScript throws an error, which by default would cause the software to crash).
+
+
+// typeof item shows 'undefined' 
