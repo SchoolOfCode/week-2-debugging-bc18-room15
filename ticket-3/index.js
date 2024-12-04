39afe2e615dc34098b47151d91a9fc03c2296745
Timothy Li <timothy93li@gmail.com>
2024-12-04 14:05:59 +0000

    finished ticket 4, ticket 3 unresolved

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -8,5 +8,21 @@ function countNumberOfNinesInNumber(number) {
     }
   }
 
-  return count;
+//  return count;
+}
+
+// declaring function called countNumberOfNinesInNumber, which takes in a parameter called number
+// define a variable called count to track the number of 9s in number, starting at 0
+
+// for loop, iterates through number
+// declaring constant variable called digit
+// 
+
+console.log(countNumberOfNinesInNumber(99.999));
+
+
+let word = [1, 2, 3];
+
+for (let i = 0; i < word.length; i++) {
+  console.log(i);
 }
--- ticket-4/index.js
+++ ticket-4/index.js
@@ -1,15 +1,18 @@
 function getGradeFromPoints(points) {
-  if ((points) => 90) {
+  if (points >= 90) {
     return "A";
-  } else if (Points >= 70) {
+  } else if (points >= 70) {
     return "B";
-  } else if (pointss >= 50) {
+  } else if (points >= 50) {
     return "C";
-  } else if (poInts >= 40) {
+  } else if (points >= 40) {
     return "D";
   }
   return "E";
 }
 
-const grade = getGradeFromPoints(90);
-console.log(Grade, "should be A");
+let grade = getGradeFromPoints(40);
+console.log(grade);
+
+
+// to debug, fixed operators and changed 'const grade' to 'let grade'
